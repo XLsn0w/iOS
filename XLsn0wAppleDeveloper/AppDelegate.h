c9be0cf589a5330e232789777913f7cbd214bc73
XLsn0w <xlsn0w@outlook.com>
2017-07-09 17:08:05 +0800

    init

---
+++ XLsn0wAppleDeveloper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLsn0wAppleDeveloper
+//
+//  Created by XLsn0w on 2017/7/9.
+//  Copyright © 2017年 XLsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XLsn0wAppleDeveloper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLsn0wAppleDeveloper
+//
+//  Created by XLsn0w on 2017/7/9.
+//  Copyright © 2017年 XLsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
