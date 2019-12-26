49a498f84204cb47f818906f332048d14354fd65
XLsn0w <XLsn0w@gmail.com>
2019-12-26 16:49:50 +0800

    add

--- LookForJob/LookForJob/ViewController.m
+++ LookForJob/LookForJob/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSTimer* timer;
+
 @end
 
 @implementation ViewController
@@ -18,13 +20,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    NSDefaultRunLoopMode
+    //NSDefaultRunLoopMode
     
 //    Default（NSDefaultRunLoopMode）：默认，一般情况下使用；
 //    Connection（NSConnectionReplyMode）：一般系统用来处理NSConnection相关事件，开发者一般用不到；
 //    Modal（NSModalPanelRunLoopMode）：处理modal panels事件；
 //    Event Tracking（NSEventTrackingRunLoopMode）：用于处理拖拽和用户交互的模式。
 //    Common（NSRunloopCommonModes）：模式合集。默认包括Default，Modal，Event Tracking三大模式，可以处理几乎所有事件。
+    
+    // 方法1
+    NSTimer* timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(repeat:) userInfo:nil repeats:true];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+
+    // 方法2
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(repeat:) userInfo:nil repeats:true];
+        [[NSRunLoop currentRunLoop] run];
+    });
 }
 
 
