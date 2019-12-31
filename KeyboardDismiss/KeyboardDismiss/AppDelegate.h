352001a8f5e1a15c566b234e356c5cffbea5d40b
X <x@gmail.com>
2019-12-31 10:59:19 +0800

    add

---
+++ KeyboardDismiss/KeyboardDismiss/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KeyboardDismiss
+//
+//  Created by mac on 2019/12/30.
+//  Copyright © 2019 XLsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ KeyboardDismiss/KeyboardDismiss/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  KeyboardDismiss
+//
+//  Created by mac on 2019/12/30.
+//  Copyright © 2019 XLsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
